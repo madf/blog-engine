7be5c9c1f0f0c6c35222c0064752398318fb84a3
Maksym Mamontov <madf@madf.info>
2025-10-06 17:23:56 +0300

    Add command line arguments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,74 @@
 module Main (main) where
 
-import System.Environment (getArgs)
-import Data.Maybe
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitSuccess, exitWith, ExitCode(..))
 import Data.Text
 import Madf.Blog
 import Madf.Blog.Env
 
+version :: String
+version = "0.1.0.0"
+
+printVersion :: IO ()
+printVersion = putStrLn $ "mbe version " ++ version
+
+printHelp :: IO ()
+printHelp = do
+    progName <- getProgName
+    putStrLn $ "Usage: " ++ progName ++ " [OPTIONS]"
+    putStrLn ""
+    putStrLn "Options:"
+    putStrLn "  -h, --help              Show this help message"
+    putStrLn "  -v, --version           Show version information"
+    putStrLn "  -c, --config FILE       Path to configuration file"
+    putStrLn "      --regen-key         Regenerate JWT key (invalidates all tokens)"
+    putStrLn ""
+    putStrLn "If no config file is specified, uses default configuration."
+
+data RunOptions = RunOptions
+    { configFile  :: !(Maybe FilePath)
+    , regenKey    :: !Bool
+    }
+
+defaultRunOptions :: RunOptions
+defaultRunOptions = RunOptions
+    { configFile = Nothing
+    , regenKey = False
+    }
+
+data Action
+    = ShowHelp
+    | ShowVersion
+    | Run RunOptions
+    | ParseError String
+
+parseArgs :: [String] -> Action
+parseArgs = parseArgs' defaultRunOptions
+  where
+    parseArgs' :: RunOptions -> [String] -> Action
+    parseArgs' opts [] = Run opts
+    parseArgs' _ ("-h":_) = ShowHelp
+    parseArgs' _ ("--help":_) = ShowHelp
+    parseArgs' _ ("-v":_) = ShowVersion
+    parseArgs' _ ("--version":_) = ShowVersion
+    parseArgs' opts ("-c":path:rest) = parseArgs' (opts { configFile = Just path }) rest
+    parseArgs' opts ("--config":path:rest) = parseArgs' (opts { configFile = Just path }) rest
+    parseArgs' opts ("--regen-key":rest) = parseArgs' (opts { regenKey = True }) rest
+    parseArgs' _ ["-c"] = ParseError "Option -c requires an argument"
+    parseArgs' _ ["--config"] = ParseError "Option --config requires an argument"
+    parseArgs' _ (unknown:_) = ParseError $ "Unknown option: " ++ unknown
+
 main :: IO ()
 main = do
-    mfn <- listToMaybe <$> getArgs
-    env <- maybe defaultEnv (create . pack) mfn
-    serve env
+    args <- getArgs
+    case parseArgs args of
+        ShowHelp -> printHelp >> exitSuccess
+        ShowVersion -> printVersion >> exitSuccess
+        Run opts -> do
+            env <- maybe defaultEnv (create . pack) (configFile opts)
+            -- TODO: implement JWT key regeneration using regenKey opts
+            serve env
+        ParseError err -> do
+            putStrLn $ "Error: " ++ err
+            putStrLn "Use --help for usage information"
+            exitWith (ExitFailure 1)
